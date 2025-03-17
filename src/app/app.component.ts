be138de953dde939324c57c090da1a925464b0d7
bezalelvm23 <bezalelvm17@gmail.com>
2025-03-18 01:21:52 +0530

    reload page when switching mobile and desktop view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { TeamDashboardComponent } from './team/team-dashboard/team-dashboard.component';
 import { DashboardComponent } from './shared/dashboard/dashboard.component';
 import { TasksDashboardComponent } from './tasks/tasks-dashboard/tasks-dashboard.component';
+import debounce from 'debounce';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +13,40 @@ import { TasksDashboardComponent } from './tasks/tasks-dashboard/tasks-dashboard
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  host: {
+    '(window:resize)': 'onDebounce($event)',
+  },
 })
 export class AppComponent {
   mode: 'team' | 'tasks' = 'team';
+  reloaded: { mobile: boolean; desktop: boolean };
 
+  constructor() {
+    let reloadData = localStorage.getItem('reload');
+    if (reloadData) {
+      this.reloaded = JSON.parse(reloadData);
+    } else {
+      this.reloaded = { mobile: false, desktop: false };
+    }
+  }
+
+  onDebounce = debounce(this.onResize, 500);
+  onResize(event: Event) {
+    let screenWidth = window.innerWidth;
+
+    if (screenWidth <= 768 && !this.reloaded.mobile) {
+      this.reloaded.mobile = true;
+      this.reloaded.desktop = false;
+      localStorage.setItem('reload', JSON.stringify(this.reloaded));
+      location.reload();
+    }
+    if (screenWidth > 768 && !this.reloaded.desktop) {
+      this.reloaded.desktop = true;
+      this.reloaded.mobile = false;
+      localStorage.setItem('reload', JSON.stringify(this.reloaded));
+      location.reload();
+    }
+  }
   changeMode() {
     this.mode = this.mode === 'team' ? 'tasks' : 'team';
   }
